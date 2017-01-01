ffab046021a6a2ccb6b4e8ba74448d1478c3bcf3
c_aarutyunyan <c_aarutyunyan@groupon.com>
2016-12-31 17:11:32 -0800

    first commit

---
+++ app/src/main/res/asdf.java
@@ -0,0 +1,13 @@
+/**
+ * Created by c_aarutyunyan on 12/31/16.
+ */
+public class asdf {
+    private static asdf ourInstance = new asdf();
+
+    public static asdf getInstance() {
+        return ourInstance;
+    }
+
+    private asdf() {
+    }
+}

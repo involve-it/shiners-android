e33de53cc024b460b1bab6de7bda3f9ff71f2388
Евгений Ширяев <xvolkx48@gmail.com>
2018-03-02 17:10:53 +0300

    Add Class UserProfile for user.profile{city, phone, inviteCode, role}

--- app/src/main/java/org/buzzar/appnative/logic/objects/User.java
+++ app/src/main/java/org/buzzar/appnative/logic/objects/User.java
@@ -2,12 +2,14 @@ package org.buzzar.appnative.logic.objects;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.provider.ContactsContract;
 
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -30,21 +32,34 @@ public class User implements Parcelable, Serializable {
     public Photo image;
     @SerializedName("profileDetails")
     public List<ProfileDetail> profileDetails;
+
+    @SerializedName("profile")
+    public UserProfile profile;
     public Date lastMobileLocationReport;
     public boolean enableNearbyNotifications;
     public boolean isInvisible;
 
+    @SerializedName("test")
+    public String test;
+
+
+
     @SerializedName("emails")
     public List<ProfileEmail> emails;
+
+
 
     public User (){}
 
     protected User(Parcel in) {
+
         _id = in.readString();
         username = in.readString();
         online = in.readByte() != 0;
+        profile = in.readParcelable(UserProfile.class.getClassLoader());
         image = in.readParcelable(Photo.class.getClassLoader());
         profileDetails = in.createTypedArrayList(ProfileDetail.CREATOR);
+        test=in.readString();
         long temp = in.readLong();
         if (temp != 0){
             this.createdAt = new Date(temp);
@@ -61,6 +76,8 @@ public class User implements Parcelable, Serializable {
         isInvisible = in.readByte() != 0;
 
         emails = in.createTypedArrayList(ProfileEmail.CREATOR);
+
+
     }
 
     public void deleteProfileDetail(String key){
@@ -126,13 +143,11 @@ public class User implements Parcelable, Serializable {
 
     public static final Creator<User> CREATOR = new Creator<User>() {
         @Override
-        public User createFromParcel(Parcel in) {
-            return new User(in);
+        public User createFromParcel(Parcel in) {return new User(in);
         }
 
         @Override
-        public User[] newArray(int size) {
-            return new User[size];
+        public User[] newArray(int size) {return new User[size];
         }
     };
 
@@ -178,6 +193,47 @@ public class User implements Parcelable, Serializable {
         parcel.writeByte((byte) (enableNearbyNotifications ? 1 : 0));
         parcel.writeByte((byte) (isInvisible ? 1 : 0));
         parcel.writeTypedList(emails);
+    }
+
+    public static class  UserProfile implements Parcelable, Serializable {
+        @SerializedName("phone")
+        public String phone;
+        @SerializedName("inviteCode")
+        public String inviteCode;
+        @SerializedName("city")
+        public String city;
+        @SerializedName("role")
+        public String role;
+
+        UserProfile(){}
+
+        protected UserProfile(Parcel in){
+            phone = in.readString();
+            inviteCode = in.readString();
+            city = in.readString();
+            role = in.readString();
+        }
+
+        public static final Creator<UserProfile> CREATOR = new Creator<UserProfile>() {
+            @Override
+            public UserProfile createFromParcel(Parcel in) {return new UserProfile(in);}
+
+            @Override
+            public UserProfile[] newArray(int size) {return new UserProfile[size];}
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            parcel.writeString(phone);
+            parcel.writeString(inviteCode);
+            parcel.writeString(city);
+            parcel.writeString(role);
+        }
     }
 
     public static class ProfileDetail implements Parcelable, Serializable{
